6e147c57f6e21351fb85db49a5bb62b9e924a1af
AlfaridzaHamdani <alfaridzah@gmail.com>
2024-10-22 20:48:34 +0700

    Add

---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <>
+      <div>
+        <h1>Dashboard</h1>
+        <p>Welcome to the dashboard!</p>
+      </div>
+
+      <a href="/maps">Maps</a>
+    </>
+  );
+};
+
+export default Dashboard;
